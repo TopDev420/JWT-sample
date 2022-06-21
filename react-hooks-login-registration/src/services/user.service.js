36ccaaaa36fb52f7b0a9ba0fe2b5bb367c7b4479
React-teaching <e1bz6canabis666.8888799ol@gmail.com>
2022-06-21 21:42:50 +0900

    front end for JST MERN stack sample project

---
+++ react-hooks-login-registration/src/services/user.service.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/api/test/";
+const getPublicCotent = () => {
+    return axios.get(API_URL + "all");
+};
+const getUserBoard = () => {
+    return axios.get(API_URL + "user");
+};
+const getModeratorBoard = () => {
+    return axios.get(API_URL + "mod");
+};
+const getAdminBoard = () => {
+    return axios.get(API_URL + "admin");
+};
+const UserService = {
+    getPublicCotent,
+    getUserBoard,
+    getModeratorBoard,
+    getAdminBoard,
+}
+export default UserService;
